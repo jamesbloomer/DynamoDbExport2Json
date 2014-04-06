96761460744cd594e1b97ad4d5f62a5f4e7969fc
James <github2@thebloomers.co.uk>
2014-04-06 20:22:27 +0100

    almost correct JSON string

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ var es = require("event-stream"),
     fs = require('fs');
 
 module.exports = function(exportFilePath, outputFilePath, done) {
-    fs.createReadStream('test/test-file.txt')
+
+    var s = fs.createReadStream(exportFilePath)
         .pipe(es.replace(String.fromCharCode(3), ':')) // ETX
         .pipe(es.replace(String.fromCharCode(2), ',')) // STX
+        .pipe(es.replace('{"n":', ''))
+        .pipe(es.replace('{"s":', ''))
+        .pipe(es.replace('}', ''))
         .pipe(es.split("\n"))
         .pipe(es.mapSync(function(data) {
-            return '{' + data + '}';
+            if(data.length > 0) {
+                return '{' + data + '}';
+            }
          }))
         .pipe(es.join(","))
-        //.pipe(es.replace(',{}'), '')
-        //.pipe(es.wait())
-        //.pipe(es.mapSync(function(data) {
-        //    return '{' + data + '}';
-        // }))
+        .pipe(es.wait())
+        .pipe(es.mapSync(function(data) {
+            return '[' + data + ']';
+         }))
         .pipe(fs.createWriteStream(outputFilePath));
-    return done();
+
+    return s.on('close', done);
 };
--- test/index_test.js
+++ test/index_test.js
@@ -8,33 +8,16 @@ describe('dynamoDbExport2Json', function() {
         if(fs.existsSync('output.json')) {
             fs.unlinkSync('output.json');
         } 
-
-        if(fs.existsSync('output2.json')) {
-            fs.unlinkSync('output2.json');
-        } 
-    });
-
-    it('should run tests', function() {
-        console.log('running tests');
-    });
-
-    it('should copy simple text file', function(done) {
-        var expected = 'file contents\n';    
-        convert('simple-test-file.txt', 'test/output1.json', function(e) {
-            fs.readFile('test/output1.json', 'utf8', function(e2, data) {
-                assert.equal(data, expected);
-                return done();
-            });
-        });
     });
 
     it('should convert dynamodb output to json', function(done) {
-        var expected = '{}';    
-        convert('test-file.txt', 'test/output2.json', function(e) {
-            fs.readFile('test/output2.json', 'utf8', function(e2, data) {
-                assert.equal(data, expected);
-                return done();
-            });
+        var expected = '[{column1:"1",column2:"abc",column3:"a",column4:"1"},{column1:"2",column2:"def",column3:"b",column4:"2"},{column1:"3",column2:"ghi",column3:"c",column4:"3"},{column1:"4",column2:"jkl",column3:"d",column4:"4"},{column1:"5",column2:"mno",column3:"e",column4:"5"},{column1:"6",column2:"pqr",column3:"f",column4:"6"},{column1:"7",column2:"stu",column3:"g",column4:"7"},{column1:"8",column2:"vwx",column3:"h",column4:"8"},{column1:"9",column2:"yza",column3:"i",column3:"9"}]';    
+        convert('test/test-file.txt', 'test/output.json', function(e) {
+            var data = fs.readFileSync('test/output.json', 'utf8');
+            assert.equal(data, expected);
+            //var obj = JSON.parse(data);
+            //assert.equal(data.length, 9);
+            return done();
         });
     });
 });
